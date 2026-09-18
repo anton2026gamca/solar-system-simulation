04c84b4d13a874058d8f841fcbd2c8676017cd01
Anton Abraham <abraham1@gamca.sk>
2026-09-18 16:33:06 +0200

    Updates

--- _next/static/zVA7itfyWRKajHm4k_Pdo/_buildManifest.js
+++ _next/static/zVA7itfyWRKajHm4k_Pdo/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/solar-system-simulation//_next/:path+",
-        "destination": "/solar-system-simulation/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
